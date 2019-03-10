ebc6b484ca16e3ba160d47f4d0ee0cd67fdfb6c5
turov <turov.i@selectel.ru>
2019-03-10 19:20:57 +0300

    🍺

--- src/create-trip-point.js
+++ src/create-trip-point.js
@@ -1,20 +1,19 @@
-export default () => {
+export default (point) => {
   const tripPoint = document.createElement(`article`);
   tripPoint.classList.add(`trip-point`);
-  tripPoint.innerHTML = ` <i class="trip-icon">🚕</i>
-  <h3 class="trip-point__title">Taxi to Airport</h3>
+  tripPoint.innerHTML = ` <i class="trip-icon">${point.type}</i>
+  <h3 class="trip-point__title">${point.title}</h3>
   <p class="trip-point__schedule">
-    <span class="trip-point__timetable">10:00&nbsp;&mdash; 11:00</span>
+    <span class="trip-point__timetable">${point.time}</span>
     <span class="trip-point__duration">1h 30m</span>
   </p>
-  <p class="trip-point__price">&euro;&nbsp;20</p>
+  <p class="trip-point__price">&euro;&nbsp;${point.price}</p>
   <ul class="trip-point__offers">
-    <li>
-      <button class="trip-point__offer">Order UBER +&euro;&nbsp;20</button>
-    </li>
-    <li>
-      <button class="trip-point__offer">Upgrade to business +&euro;&nbsp;20</button>
-    </li>
+    ${point.offers.map((element) => {
+    return `<li>
+      <button class="trip-point__offer">${element} +&euro;&nbsp;20</button>
+    </li>`;
+  }).join(``)}
   </ul>`;
   return tripPoint;
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {getRandomInteger, tripFilter, tripContainer, FILTER_PROPS} from './utils';
 import createFilter from './create-filter';
 import createTripPoint from './create-trip-point';
+import {generatePoint} from './data';
 
 // Запускаем цикл рендера фильтров
 FILTER_PROPS.forEach((element) => {
@@ -8,27 +9,36 @@ FILTER_PROPS.forEach((element) => {
   tripFilter.appendChild(filter);
 });
 
-// Функция заполнения точками путешествия
-const fillTripContaier = (count) => {
+
+// Функция генерации массива с данными
+const makePoints = (count) => {
+  const points = [];
   for (let i = 0; i < count; i++) {
-    const point = createTripPoint();
-    tripContainer.appendChild(point);
+    points.push(generatePoint());
   }
+  return points;
+};
+
+// Функция создания карточек по данным
+const fillTripContainer = (points) => {
+  points.forEach((element)=>{
+    tripContainer.appendChild(createTripPoint(element));
+  });
 };
 
 // Заполняем контейнер 7-ю событиями
-fillTripContaier(7);
+fillTripContainer(makePoints(4));
 
 // Функция обнуления доски и её заполнения случайным количеством трип поинтов (от 1 до 7)
-const generatePoints = () => {
+const fillPoints = () => {
   tripContainer.innerHTML = ``;
-  fillTripContaier(getRandomInteger(1, 7));
+  fillTripContainer(makePoints(getRandomInteger(1, 7)));
 };
 
 // Находим в DOM фильтры
 const filterLabels = document.body.querySelectorAll(`.trip-filter__item`);
 
-// Навешиваем события клика на каждый фильтр по которому вызывается функция generatePoints
+// Навешиваем события клика на каждый фильтр по которому вызывается функция fillPoints
 filterLabels.forEach((element) => {
-  element.addEventListener(`click`, generatePoints);
+  element.addEventListener(`click`, fillPoints);
 });
